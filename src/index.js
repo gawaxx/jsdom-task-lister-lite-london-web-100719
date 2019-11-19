c01ffe9fa0a8ecff4010a647ef81d612a5169c88
gawaxx <jules.blanc@unccub.com>
2019-11-19 10:58:28 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let form = document.querySelector('#create-task-form');
+  let textBox = document.querySelector('#new-task-description');
+  let tasks = document.querySelector('#tasks');
+  let submitButton = document.querySelector("input[type='submit']")
+
+  submitButton.addEventListener('click', handleForm);
+
+  function handleForm(event) {
+    event.preventDefault();
+    createListItem()
+  }
+
+  function createListItem() {
+    let li = document.createElement('li');
+    li.innerHTML = textBox.value;
+    let deleteButton = document.createElement('BUTTON');
+    deleteButton.innerHTML = 'X';
+    deleteButton.addEventListener('click', deleteListItem);
+    li.appendChild(deleteButton);
+    tasks.appendChild(li);
+  }
+
+  function deleteListItem(event) {
+    event.target.parentNode.parentNode.removeChild(event.target.parentNode)
+  }
+
+
 });
